e51a6e0a436a263b6593d605743c72ad31057a8f
Kevin Park <kevypark@gmail.com>
2020-05-31 16:46:46 -0400

    formatted page- prettier

--- public/src/App.js
+++ public/src/App.js
@@ -1,61 +1,63 @@
-import React, { useEffect } from 'react';
-import { hot } from 'react-hot-loader/root';
+import React, { useEffect } from "react";
+import { hot } from "react-hot-loader/root";
 import HeaderContainer from "./containers/HeaderContainer";
 import BodyContainer from "./containers/BodyContainer";
 import FooterContainer from "./containers/FooterContainer";
 
 import MobileHeaderContainer from "./containers/MobileHeaderContainer";
 
-
-
-
 const debounce = (fn, ms) => {
-  let timer
-  return _ => {
-    clearTimeout(timer)
-    timer = setTimeout(_ => {
-      timer = null
-      fn.apply(this, arguments)
-    }, ms)
+  let timer;
+  return (_) => {
+    clearTimeout(timer);
+    timer = setTimeout((_) => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
   };
-}
+};
 
-const App = props => {
-  const [state, setWidth] = React.useState({ 
-    isMobile: window.innerWidth <= 1022 ? true : false
-  })
-  
+const App = (props) => {
+  const [state, setWidth] = React.useState({
+    isMobile: window.innerWidth <= 1022 ? true : false,
+  });
 
   useEffect(() => {
-   const debouncedHandleResize = debounce(() => {
-      if(window.innerWidth <= 1022) {
+    const debouncedHandleResize = debounce(() => {
+      if (window.innerWidth <= 1022) {
         setWidth({
-          isMobile: true
-        })
+          isMobile: true,
+        });
       } else if (window.innerWidth > 1022) {
         setWidth({
-          isMobile: false
-        })
+          isMobile: false,
+        });
       }
-    }, 300)
-   return window.addEventListener('resize', debouncedHandleResize)
-})
-  
+    }, 300);
+    return window.addEventListener("resize", debouncedHandleResize);
+  });
+
   const renderView = () => {
     if (state.isMobile === true) {
-      console.log('mobile')
-      return <div><MobileHeaderContainer/></div>
+      console.log("mobile");
+      return (
+        <div>
+          <MobileHeaderContainer />
+        </div>
+      );
     } else {
-      console.log('browser', window.innerWidth)
+      console.log("browser", window.innerWidth);
       // console.log('WIDTH: ', state.width)
-      return <div><HeaderContainer/><BodyContainer/><FooterContainer/></div>
+      return (
+        <div>
+          <HeaderContainer />
+          <BodyContainer />
+          <FooterContainer />
+        </div>
+      );
     }
-  }
-  return (
-    <div style={{maxHeight: '1300px'}}>
-      {renderView()}
-    </div>
-    ) 
-}
+  };
+  return <div style={{ maxHeight: "1300px" }}>{renderView()}</div>;
+};
 
 export default hot(App);
--- public/src/components/MobileHeader.jsx
+++ public/src/components/MobileHeader.jsx
@@ -3,59 +3,64 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    background: "black"
+    background: "black",
   },
   button: {
     color: "red",
     size: "medium",
-    marginLeft: '8px',
-    marginRight: '8px',
-    fontWeight: 'bold',
-    fontSize: '15px'
+    marginLeft: "8px",
+    marginRight: "8px",
+    fontWeight: "bold",
+    fontSize: "15px",
   },
   menuButton: {
-    color:'grey'
-  }
+    color: "grey",
+  },
+  tab: {
+    color: "red",
+    fontWeight: "bold",
+    marginLeft: "5px",
+    marginRight: "5px",
+  },
 }));
 
-const MobileHeader = props => {
+const MobileHeader = (props) => {
   const classes = useStyles();
   return (
-    <div
-      className={classes.root}
-    >
-    <AppBar style={{background:'black'}} >
-    <Tabs  aria-label="simple tabs example">
-    <IconButton edge="start" color="inherit" aria-label="menu" style={{ marginLeft: '10px'}}>
-    <MenuIcon className={classes.menuButton}/>
-  </IconButton>
-        <Tab label="HOME"  style={{color:'red'}}/>
-        <Tab label="ABOUT"  style={{color:'red'}}/>
-        <Grid
-        container
-        justify="flex-end"
-        alignItems="center"
-        style={{}}
-      >
-        <Button
-          aria-controls="simple-menu"
-          aria-haspopup="true"
-          onClick={() => {}}
-        >
-        <ShoppingCartIcon style={{ color:'red'}}/>
-        </Button>
-      </Grid>
-      </Tabs>   
-</AppBar>
+    <div className={classes.root}>
+      <AppBar style={{ background: "black" }}>
+        <Tabs aria-label="simple tabs example">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            style={{ marginLeft: "10px" }}
+          >
+            <MenuIcon className={classes.menuButton} />
+          </IconButton>
+          <Tab label="HOME" className={classes.tab} />
+          <Tab label="ABOUT" className={classes.tab} />
+          <Tab label="CONTACT US" className={classes.tab} />
+          <Grid container justify="flex-end" alignItems="center" style={{}}>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={() => {}}
+            >
+              <ShoppingCartIcon style={{ color: "red" }} />
+            </Button>
+          </Grid>
+        </Tabs>
+      </AppBar>
     </div>
   );
 };
