e3e066e2b6ca3c610e97fa11baa02b08f24bd398
Kevin Park <kevypark@gmail.com>
2020-05-31 17:42:03 -0400

    added rightside drawer on checkout click

--- public/src/components/Header.jsx
+++ public/src/components/Header.jsx
@@ -5,73 +5,144 @@ import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Drawer from "@material-ui/core/Drawer";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     background: "#50C878",
-    margin: '-10px'
+    margin: "-10px",
   },
   button: {
     color: "black",
     size: "medium",
-    marginLeft: '8px',
-    marginRight: '8px',
-    fontWeight: 'bold',
-    fontSize: '15px',
-    marginBottom: '20px'
+    marginLeft: "8px",
+    marginRight: "8px",
+    fontWeight: "bold",
+    fontSize: "15px",
+    marginBottom: "20px",
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
   },
 }));
 
-const Header = props => {
+const Header = (props) => {
   const classes = useStyles();
-  return (
+  const [state, setState] = React.useState({
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+  const list = (anchor) => (
     <div
-      className={classes.root}
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
     >
-    <AppBar />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <div className={classes.root}>
+      <AppBar />
       <Toolbar>
         <Button
-            className={classes.button}
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={() => {}}
-          >HOME</Button>
-       
-          <Button
           className={classes.button}
           aria-controls="simple-menu"
           aria-haspopup="true"
           onClick={() => {}}
-        >LOOKBOOK</Button>
-        <Button
-          className={classes.button}
-          aria-controls="simple-menu"
-          aria-haspopup="true"
-          onClick={() => {}}
-        >FAQS</Button>
-        <Button
-          className={classes.button}
-          aria-controls="simple-menu"
-          aria-haspopup="true"
-          onClick={() => {}}
-        >ABOUT</Button>
-        <Grid
-          container
-          justify="flex-end"
-          alignItems="center"
-          style={{}}
         >
+          HOME
+        </Button>
+
+        <Button
+          className={classes.button}
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={() => {}}
+        >
+          LOOKBOOK
+        </Button>
+        <Button
+          className={classes.button}
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={() => {}}
+        >
+          FAQS
+        </Button>
+        <Button
+          className={classes.button}
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={() => {}}
+        >
+          ABOUT
+        </Button>
+        <Grid container justify="flex-end" alignItems="center" style={{}}>
           <Button
             aria-controls="simple-menu"
             aria-haspopup="true"
-            onClick={() => {}}
+            onClick={toggleDrawer("right", true)}
           >
-            <ShoppingCartIcon style={{marginBottom: '20px'}}/>
+            <ShoppingCartIcon style={{ marginBottom: "20px" }} />
           </Button>
         </Grid>
       </Toolbar>
+      <div>
+        <Drawer
+          anchor={"right"}
+          open={state["right"]}
+          onClose={toggleDrawer("right", false)}
+        >
+          {list("right")}
+        </Drawer>
+      </div>
     </div>
   );
 };
