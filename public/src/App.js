d37c00f44d3be10faa6248f25e1ef414419696fd
Kevin Park <kevypark@gmail.com>
2020-06-14 15:16:37 -0400

    prettiered app.js

--- public/src/App.js
+++ public/src/App.js
@@ -3,6 +3,7 @@ import { hot } from "react-hot-loader/root";
 import HeaderContainer from "./containers/HeaderContainer";
 import BodyContainer from "./containers/BodyContainer";
 import FooterContainer from "./containers/FooterContainer";
+
 import MobileHeaderContainer from "./containers/MobileHeaderContainer";
 import MobileBodyContainer from "./containers/MobileBodyContainer";
 
