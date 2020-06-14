396b0e443ddd2e26e875961bfeb95be75a87c756
Kevin Park <kevypark@gmail.com>
2020-06-14 16:10:13 -0400

    continuing to alter naming convention to product rather than merch

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -3,14 +3,14 @@ import { combineReducers } from 'redux';
 // import below your specified reducers to create a link to the rootReducer (aka. your store)
 import entry from './todoEntryReducer';
 import todos from './todosReducer';
-import merchClicked from './merchClickedReducer';
+import productClicked from './productClickedReducer';
 
 
 const rootReducers = combineReducers({
     // add reducer file references here
     entry,
     todos,
-    merchClicked
+    productClicked
 });
 
 export default rootReducers;
